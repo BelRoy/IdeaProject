19a85c3e3f9b3840d5a225ae73c5e5360d132f50
BelRoy <oksiwhite@ukr.net>
2017-06-12 17:19:15 +0300

    Version 2
    Version 2

---
+++ app/src/main/java/com/devqt/idea/project/StartDisplay.java
@@ -0,0 +1,36 @@
+package com.devqt.idea.project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import start.LogIn;
+import start.Registration;
+
+public class StartDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_display);
+
+        findViewById(R.id.log_in).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartDisplay.this, LogIn.class));
+                finish();
+            }
+        });
+
+        findViewById(R.id.regis).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartDisplay.this, Registration.class));
+                finish();
+            }
+        });
+    }
+
+
+}
